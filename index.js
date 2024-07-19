c11cea8332c41df4a454ab51fdd914e8628e88e5
darthvader1925 <elijb12@gmail.com>
2024-07-19 01:37:10 -0700

    dashboard

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "html", "home.html"));
+});
 
 app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "html", "login.html"))
